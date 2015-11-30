56ac856e2cfcb9364ce1145ab372b4a165a2f343
Ossi Herrala <oherrala@gmail.com>
2015-11-30 20:04:29 +0000

    Tweak pledge further

--- README.md
+++ README.md
@@ -4,15 +4,14 @@ Haskell library for OpenBSD's pledge(2)
 
 Example Haskell program:
 
-
     import System.Pledge
     import System.IO
 
     main :: IO ()
     main = do
-         pledge [Stdio] ""
-         print "Pledge works"
-         withFile "/dev/urandom" ReadMode $ \f ->
-           hGetContents f >>= putStr
+      pledge [Stdio] []
+      print "Pledge works"
+      withFile "/dev/urandom" ReadMode $ \f ->
+        hGetContents f >>= putStr
 
 pledge(2) kills this when withFile tries to open file.
--- src/System/Pledge.hs
+++ src/System/Pledge.hs
@@ -2,17 +2,19 @@
 
 module System.Pledge (Promise(..), pledge) where
 
-import Data.List (intersperse)
+import Data.List (intersperse, nub)
 import Data.Char (toLower)
-
-import Foreign
+import Foreign (Ptr, nullPtr)
 import Foreign.C.Error (throwErrnoIfMinus1_)
 import Foreign.C.String (CString, withCString)
+import System.FilePath (FilePath)
 
-foreign import ccall "unistd.h pledge" c_pledge :: CString -> CString -> IO Int
+foreign import ccall "unistd.h pledge" c_pledge :: CString -> Ptr [CString] -> IO Int
 
+-- | List of promises to give
 type Promises = [Promise]
 
+-- | Allowed promises. See OpenBSD's pledge(2) for documentation
 data Promise = Rpath
              | Wpath
              | Cpath
@@ -43,14 +45,51 @@ data Promise = Rpath
              | None
              deriving (Eq, Show)
 
+
+-- | Pledge a program
+pledge :: Promises -> [FilePath] -> IO ()
+
+-- special case for completely empty pledge. Useful? Maybe not.
+pledge [] _ = throwErrnoIfMinus1_ "pledge" $ c_pledge nullPtr nullPtr
+
+-- Generic case, but we don't support giving whilelist of paths yet
+pledge proms [] =
+  withCString (promises proms) $ \c_proms ->
+      let c_paths = nullPtr in
+            throwErrnoIfMinus1_ "pledge" $ c_pledge c_proms c_paths
+
+pledge _ _ = error "pledge does not support [FilePath] yet"
+
+
+-- | Convert Promise into String
+--
+-- >>> promise Stdio
+-- "stdio"
 promise :: Promise -> String
 promise = map toLower . show
 
-promises :: Promises -> String
-promises = concat . intersperse " " . map promise
 
-pledge :: Promises -> String -> IO ()
-pledge proms paths =
-  withCString (promises proms) $ \c_proms ->
-      withCString paths $ \c_paths ->
-            throwErrnoIfMinus1_ "pledge" $ c_pledge c_proms c_paths
+-- | Convert list of Promises into single String
+--
+-- >>> promises [Stdio]
+-- "stdio"
+--
+-- >>> promises [Stdio, Coredump]
+-- "stdio coredump"
+--
+-- >>> promises [Stdio, Coredump, Coredump, Stdio]
+-- "stdio coredump"
+--
+-- >>> promises []
+-- ""
+--
+-- >>> promises [None]
+-- ""
+--
+-- >>> promises [Stdio, Coredump, None]
+-- ""
+promises :: Promises -> String
+promises [] = ""
+promises proms = if elem None proms
+                 then ""
+                 else concat $ intersperse " " $ map promise $ nub proms
